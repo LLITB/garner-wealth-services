d33a9c4efe894c7fc1d6e3623e5b6c4470144139
Tyus Durant <contact@tyusdurant.com>
2024-12-01 19:47:59 -0500

    ids to sections

--- src/lib/slices/ImageCards/index.svelte
+++ src/lib/slices/ImageCards/index.svelte
@@ -39,7 +39,7 @@
 		class=" px-4 w-full bg-[#F6F8FB]"
 		data-slice-type={slice.slice_type}
 		data-slice-variation={slice.variation}
-		id={slice.primary.section_id ? `${slice.primary.section_id}` : 'none'}
+		id="expertise"
 	>
 		<div class="grid gap-6 md:gap-12 py-5 md:py-10 max-w-7xl mx-auto">
 			{#if isFilled.richText(slice.primary.heading)}
@@ -63,7 +63,7 @@
 		class="bg-white max-w-7xl mx-auto px-8 pt-12 pb-6"
 		data-slice-type={slice.slice_type}
 		data-slice-variation={slice.variation}
-		id={slice.primary.section_id ? `${slice.primary.section_id}` : 'none'}
+		id="services"
 	>
 		<div class="grid gap-12">
 			{#if isFilled.richText(slice.primary.heading)}
@@ -85,7 +85,7 @@
 		class="flex flex-col gap-4 py-6 lg:py-12 md:justify-around w-full max-md:max-w-full bg-[#156F89]"
 		data-slice-type={slice.slice_type}
 		data-slice-variation={slice.variation}
-		id={slice.primary.section_id ? `${slice.primary.section_id}` : 'none'}
+		id="benefits"
 	>
 		<div class="grid gap-12 mx-auto max-w-7xl bg-[#156F89]">
 			{#if isFilled.richText(slice.primary.heading)}
